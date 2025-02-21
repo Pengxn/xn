c98fef9661ec9dcd7e298f6c4f1837707e1a53a2
huiyifyj <jxfengyijie@gmail.com>
2025-02-21 10:11:59 +0800

    Normalize path separator in Check-Version.ps1 file
    Replace backslash with forward slash in SCOOP_HOME path to maintain consistency.

--- bin/Check-Version.ps1
+++ bin/Check-Version.ps1
@@ -13,7 +13,7 @@ if (!$env:SCOOP_HOME) {
     $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop)
 }
 
-$checkver = "$env:SCOOP_HOME\bin\checkver.ps1"
+$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 
 $remainArgs = ($remainArgs | Select-Object -Unique) -join ' '
 
